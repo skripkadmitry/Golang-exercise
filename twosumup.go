84b2ec91bb0285dfc47295adc997a0921fadc4de
skripkadmitry <dskrippka@gmail.com>
2022-08-05 01:29:58 +0300

    better sum of two
    but less interesting

--- twosumup.go
+++ twosumup.go
@@ -1,47 +1,19 @@
 package main
 
-import (
-	"fmt"
-	"sort"
-)
-
-type Enumerate struct {
-	Index []int
-	Nums  []int
-}
-
-func (e Enumerate) Len() int {
-	return len(e.Nums)
-}
-
-func (e Enumerate) Less(i, j int) bool {
-	return e.Nums[i] < e.Nums[j]
-}
-
-func (e Enumerate) Swap(i, j int) {
-	e.Index[i], e.Index[j] = e.Index[j], e.Index[i]
-	e.Nums[i], e.Nums[j] = e.Nums[j], e.Nums[i]
-}
+import "fmt"
 
 func main() {
-	nums := []int{2, 7, 11, 15}
-	index := make([]int, len(nums), len(nums))
-	for i := 0; i < len(index); i++ {
-		index[i] = i
-	}
-	Arr := Enumerate{index, nums}
-	target := 9
-	sort.Sort(Arr)
-	i, j := 0, len(nums)-1
-	for i <= j {
-		if Arr.Nums[i]+Arr.Nums[j] > target {
-			j -= 1
-		} else if Arr.Nums[i]+Arr.Nums[j] < target {
-			i += 1
-		} else {
-			fmt.Println(Arr.Index[i], Arr.Index[j])
+	nums := []int{3, 2, 4}
+	target := 6
+	//index := make([]int, len(nums), len(nums))
+	dict := map[int]int{}
+	for i, el := range nums {
+		if res, in := dict[target-el]; in {
+			fmt.Println(i, res)
 			return
+		} else {
+			dict[el] = i
 		}
-	}
 
+	}
 }
